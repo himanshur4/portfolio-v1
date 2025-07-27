094cc08b2704ff9fba0eb30e969bbd27244a3a08
Himanshu Raj <hraj808404@gmail.com>
2025-07-27 16:00:53 +0530

    fix: update background gradient colors in App and Hero components for improved aesthetics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='flex justify-center min-h-screen dark:bg-gradient-to-br dark:from-[#000000] dark:to-[rgb(2,52,12)] bg-gradient-to-bl from-[#aeb3d6] to-[#a8e2af] font-mono font-medium'>
+    <div className='flex justify-center min-h-screen dark:bg-gradient-to-br dark:from-[#000000] dark:to-[rgb(6,78,20)] bg-gradient-to-bl from-[#ddf0e5] to-[#858171] font-mono font-medium'>
       <Home/>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
       <div className="flex flex-col items-start md:items-center md:gap-4 md:flex-row gap-2">
         <img src="/pfp13.jpg" className='w-15 sm:w-17 md:w-28 lg:w-30 rounded-full ml-2' alt="profilePic" />
         <div className="flex-col mt-1">
-          <h1 className='text-xl tracking-tight font-semibold ml-2'>Hii👋 I am <span className='bg-clip-text text-transparent bg-gradient-to-r from-[#FF512F] via-[#FF6F5E] to-[#DD2476]'>Himanshu.</span></h1>
+          <h1 className='text-xl tracking-tight font-semibold ml-2'>Hii👋 I am <span className='bg-clip-text text-transparent bg-gradient-to-r from-[#880184] via-[#640761] to-[#41064d] dark:from-[#f46e87] dark:via-[#f17d82] dark:to-[#e53cb2]'>Himanshu.</span></h1>
           <h3 className='text-lg bg-clip-text text-transparent bg-gradient-to-r dark:from-[#A1C2D4] dark:via-[#BAC8A3] dark:to-[#F1F2B5] from-[#081c58] via-[#4B545E] to-[#09313f] ml-2'>A full-stack developer</h3>
           <p className='text-sm tracking-tight sm:text-sm text-gray-900 dark:text-gray-300 mr-3 ml-2'>I transform ideas into real-world applications, from clean and responsive frontends to scalable backends. I enjoy learning new technologies, solving meaningful problems, and continuously improving my skills.</p>
           </div>
