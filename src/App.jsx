b4a0f540bd18ed3d4f82a40bc9ab8ebff5185200
Himanshu Raj <hraj808404@gmail.com>
2025-07-27 16:11:11 +0530

    fix: restore original background gradient colors in App component for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='flex justify-center min-h-screen dark:bg-gradient-to-br dark:from-[#000000] dark:to-[rgb(6,78,20)] bg-gradient-to-bl from-[#fafdd6] to-[#ebdfda] font-mono font-medium'>
+    <div className='flex justify-center min-h-screen dark:bg-gradient-to-br dark:from-[#000000] dark:to-[rgb(3,64,15)] bg-gradient-to-bl from-[#cdfce5] to-[#def2d7] font-mono font-medium'>
       <Home/>
     </div>
   )
