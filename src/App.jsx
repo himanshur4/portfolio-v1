b1a749c66c5b346f48eea3a80d03cdf5a23f0513
Himanshu Raj <hraj808404@gmail.com>
2025-08-02 21:05:37 +0530

    border in photo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 const App = () => {
   return (
     <div className='flex justify-center min-h-screen 
- font-mono font-medium dark:bg-gradient-to-br dark:from-[#0d1117] dark:via-[#18191a] dark:to-[#21262d] bg-gradient-to-bl from-[#fcfaf3] to-[#dbe4ff]'>
+ font-mono font-medium dark:bg-gradient-to-br dark:from-[#0d1117] dark:via-[#18191a] dark:to-[#21262d] bg-gradient-to-bl from-[#fcfaf3] to-[#f5ffe7]'>
       <Home/>
     </div>
   )
