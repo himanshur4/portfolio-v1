7200ce7dff94505a6a3bf39355ab0703ea206f64
Himanshu Raj <hraj808404@gmail.com>
2025-08-02 19:49:33 +0530

    bg-change-3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 const App = () => {
   return (
     <div className='flex justify-center min-h-screen 
- font-mono font-medium dark:bg-gradient-to-br dark:from-[#0d1117] dark:to-[#21262d] bg-gradient-to-bl from-[#f6f8fa] to-[#ffffff]'>
+ font-mono font-medium dark:bg-gradient-to-br dark:from-[#0d1117] dark:via-[#0e0f10] dark:to-[#25272a]  bg-gradient-to-bl from-[#f3e8ff] via-[#ddd6fe] to-[#faf5ff]'>
       <Home/>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
       <div className="flex flex-col items-start md:items-center md:gap-4 md:flex-row gap-2">
         <img src="/pfp13.jpg" className='w-15 sm:w-17 md:w-28 lg:w-30 rounded-full ml-2' alt="profilePic" />
         <div className="flex-col mt-1">
-          <h1 className='text-xl tracking-tight font-bold ml-2'>Hi, I'm <span className='bg-clip-text text-transparent bg-gradient-to-r from-[#880184] via-[#640761] to-[#41064d] dark:from-[#f46e87] dark:via-[#f17d82] dark:to-[#e53cb2]'>Himanshu!</span></h1>
-          <h3 className='text-lg bg-clip-text text-transparent bg-gradient-to-r dark:from-[#A1C2D4] dark:via-[#BAC8A3] dark:to-[#F1F2B5] from-[#081c58] via-[#4B545E] to-[#09313f] ml-2'>A full-stack developer</h3>
-          <p className='text-sm tracking-tight sm:text-sm text-gray-900 dark:text-gray-300 mr-3 ml-2'>Pre-final year Computer Science student passionate about building scalable web apps, learning new technologies, and breaking down complex ideas into clean, working solutions.</p>
+          <h1 className='text-xl font-bold ml-2'>Hi, I'm <span className='bg-clip-text text-transparent bg-gradient-to-r from-[#880184] via-[#640761] to-[#41064d] dark:from-[#f46e87] dark:via-[#f17d82] dark:to-[#e53cb2]'>Himanshu!</span></h1>
+          <h3 className='text-lg bg-clip-text text-transparent bg-gradient-to-r dark:from-[#A1C2D4] dark:via-[#BAC8A3] dark:to-[#F1F2B5] from-[#080d1a] via-[#0b0d10] to-[#051a21] ml-2'>A full-stack developer</h3>
+          <p className='text-sm sm:text-sm tracking-tight text-gray-900 dark:text-gray-300 mr-2 sm:mr-3 ml-2'>Pre-final year Computer Science student passionate about building scalable web apps, learning new technologies, and breaking down complex ideas into clean, working solutions.</p>
           </div>
       </div>
     </div>
