7208586ac8797ef83c19bb569add19fa4f472a78
Himanshu Raj <hraj808404@gmail.com>
2025-08-02 22:56:44 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 const App = () => {
   return (
     <div className='flex justify-center min-h-screen 
- font-mono font-medium dark:bg-gradient-to-br dark:from-[#0d1117] dark:via-[#18191a] dark:to-[#21262d] bg-gradient-to-bl from-[#fcfaf3] to-[#f5ffe7]'>
+ font-mono font-medium dark:bg-gradient-to-br dark:from-[#0d1117] dark:via-[#18191a] dark:to-[#21262d] bg-gradient-to-bl from-[#ecddff] to-[#d3fbf5]'>
       <Home/>
     </div>
   )
