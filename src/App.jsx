270da8df6f40269e871afcf851a90c34f5bfb468
Himanshu Raj <hraj808404@gmail.com>
2025-08-02 23:02:43 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 const App = () => {
   return (
     <div className='flex justify-center min-h-screen 
- font-mono font-medium dark:bg-gradient-to-br dark:from-[#0d1117] dark:via-[#18191a] dark:to-[#21262d] bg-gradient-to-bl from-[#ecddff] to-[#d3fbf5]'>
+ font-mono font-medium dark:bg-gradient-to-br dark:from-[#0d1117] dark:via-[#18191a] dark:to-[#21262d] bg-gradient-to-bl from-[#f6eeff] to-[#e9f2ff]'>
       <Home/>
     </div>
   )
