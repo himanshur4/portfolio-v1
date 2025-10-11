3a4f649f2aa035ed06fb5416214f51f4cf77a98c
Himanshu Raj <hraj808404@gmail.com>
2025-10-11 16:44:57 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
           <Linkedin className='rounded size-8 sm:size-9 md:size-10 p-1  cursor-pointer dark:text-gray-400 transition-transform duration-300 ease-in-out hover:scale-102 hover:bg-gray-900 hover:text-gray-50
           border border-solid dark:border-zinc-500 shadow-[2px_2px_7px_#666666] dark:shadow-[2px_2px_2px_#555555]  text-gray-950 dark:hover:bg-gray-100 dark:hover:text-gray-800 dark:bg-zinc-950' onClick={()=>handleLinkClick(data.social.linkedin)} />
         </div>
-       <a href='/resume_himanshur4.pdf'><p className='text-center font-bold hover:underline cursor-pointer mb-3 hover:scale-105 '>View Resume</p></a> 
+       <a href='/resume_himanshur4_1.pdf'><p className='text-center font-bold hover:underline cursor-pointer mb-3 hover:scale-105 '>View Resume</p></a> 
         <div className='flex items-center justify-center'>
          <ContactCard/>
         </div>
