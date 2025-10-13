1b8e76dc5433952b4e208e5e7bf14ca619dc1ca5
Himanshu Raj <hraj808404@gmail.com>
2025-10-14 01:23:17 +0530

    Refactor Experience component for improved layout and styling consistency

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -17,25 +17,24 @@ const Experience = () => {
           companies.map((c) => (<div className='flex-col bg-gray-50 dark:bg-zinc-950 border border-solid dark:border-zinc-500 shadow-[3px_4px_4px_#777777] dark:shadow-[3px_4px_3px_#555555] p-1 cursor-pointer hover:transform-3d mt-2 sm:mt-2 md:mt-3 transition-transform duration-300 ease-in-out hover:shadow-[3px_4px_0px_#000000] hover:dark:shadow-[3px_4px_0px_#aaaaaa] rounded' key={c.id}>
             <div className="flex justify-between border-b-1 border-dotted dark:border-b-gray-500">
               <div className='flex flex-col'>
-                <div className='flex items-center'>
+                <div className='flex items-center justify-start'>
                   <h1 className='text-xl font-semibold bg-clip-text text-transparent bg-gradient-to-r dark:from-[#A1C2D4] dark:via-[#BAC8A3] dark:to-[#F1F2B5]  from-[#081c58] via-[#4B545E] to-[#09313f]'>
                     {c.company}
                   </h1>
-                  <img src={c.companyLogo} className='w-5 sm:w-6 ml-2  dark:border-0' alt="company logo" />
+                  <img src={c.companyLogo} className='w-5 sm:w-6 ml-1  dark:border-0' alt="company logo" />
                 </div>
-                
-                  <span className='text-xs sm:text-sm tracking-tight dark:text-gray-300 font-normal italic'>
-                    {c.role}
-                  </span>
-                
+
+                <span className='text-xs sm:text-sm tracking-tight dark:text-gray-300 font-normal italic'>
+                  {c.role}
+                </span>
+
               </div>
               <div className='flex flex-col'>
-              <span className='text-xs sm:text-sm tracking-tight dark:text-gray-300 text-right'>
-                ({c.joining}-{c.end})
-                
-              </span>
+                <span className='text-xs tracking-tighter dark:text-gray-300 text-right'>
+                  ({c.joining}-{c.end})
+                </span>
                 <span className='text-xs sm:text-sm tracking-tight dark:text-gray-300 font-normal italic text-right mt-1'>
-                    {c.locationType}
+                  {c.locationType}
                 </span>
               </div>
             </div>
@@ -46,7 +45,7 @@ const Experience = () => {
                 </li>
               ))}
             </ul>
-            
+
 
 
           </div>
